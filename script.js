04fdfa9117d88a93004569210fa41049c4ede5e9
Heather Barr <hnbarr@yahoo.com>
2018-12-10 17:26:31 -0600

    stuck on problem

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 'use strict';
 $(document).ready(function() {
-    //User Click Variable
-    var clicks = 0; //user clicks
- 
-    //User-Click-Increase Total.
+    let clicks = 0; //user clicks.
+    let autoCost = 10; //need to make this reflect increased cost!
+    let speedCost = 100; //need to make this reflect increased cost!
+
+
+    //need to set up
+    let autoClicks = 0; //automatically click once per second, updates total.
+    let speedTime = 0; //speeds up the time for auto clicks, updates total.
+
+    // User-Click-Increase Total.
     $("#click").on("click", function(){
         clicks = clicks + 1;
         $("#clickCount").html(clicks);
     })
-    
-    //Auto Click Purchase, subtract cost.
+
+    // NOT WORKING FULLY!
+
+    // Auto Click & Speed Up Purchased = subtract cost from total!
     $("#auto").on("click", function(){
-        clicks = clicks - 10;
+        if ($("#clickCount" > (autoCost))){
+            clicks = clicks - autoCost;
+            $("#clickCount").html(clicks);
+        } else if  ($(autoCost) > ("#clickCount")){
+            console.log('Unable to purchase, you have less than cost!')
+            }//not working!
+        //need to make unable to buy if cost > total!
+    })
+    $("#speed").on("click", function(){
+        if ($("#clickCount" > (speedCost))){
+        clicks = clicks - speedCost;
         $("#clickCount").html(clicks);
+        } else; 
+        //need to make unable to buy if cost > total!
     })
 
-    // Speed Up Purchase, subtract cost.
-    $("#speed").on("click", function(){
-        clicks = clicks - 100;
-        $("#clickCount").html(clicks);
-    })
+
+
+
+
+
 
 //CREATE FUNCTIONS TO...
 // subtract from total when purchasing help
