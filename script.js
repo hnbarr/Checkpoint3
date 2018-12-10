119ee3726bef1afeb695b23406583de822ec514f
Heather Barr <hnbarr@yahoo.com>
2018-12-10 14:53:42 -0600

    updated subtraction

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 'use strict';
 $(document).ready(function() {
-    //Variable
+    //User Click Variable
     var clicks = 0; //user clicks
  
-    //Click Function
+    //User-Click-Increase Total.
     $("#click").on("click", function(){
         clicks = clicks + 1;
         $("#clickCount").html(clicks);
-    });
+    })
+    
+    //Auto Click Purchase, subtract cost.
+    $("#auto").on("click", function(){
+        clicks = clicks - 10;
+        $("#clickCount").html(clicks);
+    })
+
+    // Speed Up Purchase, subtract cost.
+    $("#speed").on("click", function(){
+        clicks = clicks - 100;
+        $("#clickCount").html(clicks);
+    })
+
+//CREATE FUNCTIONS TO...
+// subtract from total when purchasing help
+// DONE
+
+// make 'clicks' reflect the help of purchased help, help-buttons + user-clicks!
+// in process...
+
+// make help buttons cost more clicks after proir purchasing, incrementing cost!
+// haven't started yet...
+
+
 });
