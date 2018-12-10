621cc2e0aa4dd3fdb78edb7a4bbbdd646863b9da
Heather Barr <hnbarr@yahoo.com>
2018-12-10 11:06:13 -0600

    started over

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+$(document).ready(function() {
+    //Variable
+    var clicks = 0; //user clicks
+ 
+    //Click Function
+    $("#click").on("click", function(){
+        clicks = clicks + 1;
+        $("#clickCount").html(clicks);
+    });
+});
